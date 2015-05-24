c7d0ba76e764dd1a0d0656de8629b2ab64ff90a1
waykar-prashant <waykar-prashant@users.noreply.github.com>
2015-05-24 09:52:00 -0700

    Updated CRDT Client

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,19 +1,60 @@
 package edu.sjsu.cmpe.cache.client;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.hash.Funnel;
+import com.google.common.hash.Funnels;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+
+
+
+
+import java.io.IOException;
+import com.mashape.unirest.http.Unirest;
 
 public class Client {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
+	public static void main(String[] args) throws Exception {
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
+		ArrayList<CacheServiceInterface> serverList = new ArrayList<CacheServiceInterface>();
 
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
+		
+		CacheServiceInterface cacheOne = new DistributedCacheService("http://localhost:3000");
+		CacheServiceInterface cacheTwo = new DistributedCacheService("http://localhost:3001");
+		CacheServiceInterface cacheThree = new DistributedCacheService("http://localhost:3002");
+		serverList.add(cacheOne);
+		serverList.add(cacheTwo);
+		serverList.add(cacheThree);
 
-        System.out.println("Existing Cache Client...");
-    }
+		
+
+
+		CRDTClient clientCRDT = new CRDTClient(serverList);
+		clientCRDT.writeToCache(1, "a");
+
+		
+
+		System.out.println("\nTurn OFF Server A Now !\n");
+		
+		Thread.sleep(50000);
+
+			
+		clientCRDT = new CRDTClient(serverList);
+		clientCRDT.writeToCache(1, "b");
+			
+
+		System.out.println("Turn ON Server A Now");
+		Thread.sleep(50000);
+		
+		clientCRDT.readCache(1);
+		
+	}
+
+
 
 }
